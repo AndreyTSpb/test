63f091b8b73a5ea9cd0abe3324dfef9e287f53af
tynyanyi@mail.ru <qaz@qaz-Vostro-A860>
2018-03-12 17:33:12 +0300

    ajax добавлен для выборки данных

--- www/application/controllers/controller_groupsale.php
+++ www/application/controllers/controller_groupsale.php
@@ -14,25 +14,29 @@ class Controller_GroupSale extends Controller
 		$this->view->generate('groupsale_view.php', 'template_view.php', $data);
 	}
         function action_selectgroups($params = false){
-            if(!empty($params)){
-            foreach ($params as $key => $val){
-                    switch ($key){
-                        case 'i':
-                            $this->model->id_group = (int)$val;
-                            break;
-                        case 's':
-                            $this->model->id_subject = (int)$val;
-                            break;
-                        case 't':
-                            $this->model->id_type = (int)$val;
-                            break;
-                        case 'l':
-                            $this->model->id_location = (int)$val;
-                            break;
+            if($_COOKIE['id_user']){
+                if(!empty($params)){
+                foreach ($params as $key => $val){
+                        switch ($key){
+                            case 'i':
+                                $this->model->id_group = (int)$val;
+                                break;
+                            case 's':
+                                $this->model->id_subject = (int)$val;
+                                break;
+                            case 't':
+                                $this->model->id_type = (int)$val;
+                                break;
+                            case 'l':
+                                $this->model->id_location = (int)$val;
+                                break;
+                        }
                     }
                 }
+                $data['content'] = $this->model->select_groups();
+                $this->view->generate('groupsale_view.php', 'template_view.php', $data);
+            }else{
+                header("location: /");
             }
-            $data['content'] = $this->model->select_groups();
-            $this->view->generate('groupsale_view.php', 'template_view.php', $data);
         }
 }
