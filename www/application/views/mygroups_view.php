ec9a8df36c743cfb4ecc46de4e62391fd51c8ddc
Андрей <@LAPTOP>
2018-03-15 23:00:51 +0300

    Добавленны диалоги

--- www/application/views/mygroups_view.php
+++ www/application/views/mygroups_view.php
@@ -33,7 +33,7 @@ foreach ($mygroups as $id_stud => $vals){
                          if($p == 1){$style = "style ='background-color:#00CC00;'"; $pr = $month; $month ='';}
                          if($p == 2){$style = "style ='background-color:#FE2701;'"; $pr = $month; $month ='';}
                          if($p == 5){$style = "style ='background-color:#FFFF66;'"; $pr = $month; $month ='';}
-                         $pay_month .= "<div class='block_pay_month_head'>".$month."<div class='block_pay_month' ".$style.">".$pr."</div></div>";
+                         $pay_month .= "<div class='block_pay_month_head'>".$month."<div class='block_pay_month' ".$style." id='p".$i."_".$id_abon."'>".$pr."</div></div>";
                      }
                  }
                  $abons .= "<div class='abon'>
@@ -52,7 +52,7 @@ foreach ($mygroups as $id_stud => $vals){
                                             $pay_month
                                         ."<div class = 'bay_mont'>
                                             <form method='post'>
-                                                <label>Кол-во месяцев к оплате: <input type='text' name = 'for_bill' id='amount-".$id_abon."' value='1'></label>
+                                                <label>Кол-во месяцев к оплате: <input type='text' name = 'for_bill' data-id='".$id_abon."' id='amount-".$id_abon."' value='1'></label>
                                                 <label>К оплате: <input type='text' name='cost_bill' id='cost_".$id_abon."' value=''></label>
                                                 <input type='submit' name='create_bill' value='Купить'>
                                             </form>
@@ -83,8 +83,20 @@ foreach ($mygroups as $id_stud => $vals){
     </div>
 </div>
 <script>
-    $(function(){
-        $('#amount-')
-    });
+    $( "input[name=for_bill]" ).keyup(function() {
+        var kol_month = $(this).val();
+        var id_abon = $(this).data('id');
+        var p1 = Number($("#p1_"+id_abon).text());
+        var p2 = Number($("#p2_"+id_abon).text());
+        var p3 = Number($("#p3_"+id_abon).text());
+        var p4 = Number($("#p4_"+id_abon).text());
+        var p5 = Number($("#p5_"+id_abon).text());
+        var p6 = Number($("#p6_"+id_abon).text());
+        var p7 = Number($("#p7_"+id_abon).text());
+        var p8 = Number($("#p8_"+id_abon).text());
+        var p9 = Number($("#p9_"+id_abon).text());
+        $("#cost_"+id_abon).val(kol_month);
+        alert(kol_month +'='+ id_abon+'='+p1+'='+p2+'='+p3+'='+p4+'='+p5+'='+p6+'='+p7+'='+p8+'='+p9);
+       });
 </script>
 
